4261c8c8eedc344563f585aad329bc04dc59e322
Balaji R <grand.hunter.dark.15@gmail.com>
2025-05-22 18:20:10 -0400

    fix action space management to be nonbreaking

--- virtual_mcp.py
+++ virtual_mcp.py
@@ -3,7 +3,20 @@ from action_space import IssuePublicStatement
 
 class ActionSpaceManagement:
     def __init__(self):
-        self.action_space_prompt = 'Below are the avaible actions that you can do towarsd other players:\n'
+        self.base_prompt = 'Below are the available actions that you can do toward other players:\n'
+        self.action_blocks = {}  # Store each action's complete block
+    
+    @property
+    def action_space_prompt(self):
+        """Generate the full prompt by combining base prompt with all action blocks"""
+        if not self.action_blocks:
+            return self.base_prompt
+        
+        full_prompt = self.base_prompt
+        for action_block in self.action_blocks.values():
+            full_prompt += action_block
+        
+        return full_prompt
 
     def add_actions(self, list_of_actions):
         for action in list_of_actions:
@@ -13,23 +26,21 @@ class ActionSpaceManagement:
                     action_name = type(action).__name__
                     argument = ', '.join(list(function.__code__.co_varnames[:function.__code__.co_argcount][1:]))
                     description = ' '.join([d.strip() for d in function.__doc__.split('\n')])
-                    self.action_space_prompt += f"""
+                    
+                    # Store the complete action block
+                    action_block = f"""
 Action Name: {action_name}
 Action Argument: {argument}
 Action Description: {description}
 """
+                    self.action_blocks[action_name] = action_block
 
     def remove_actions(self, list_of_actions):
         for action in list_of_actions:
-            tools = inspect.getmembers(action, predicate=inspect.ismethod)
-            for function_name, function in tools:
-                if function_name == '__call__':
-                    action_name = type(action).__name__
-                    argument = ', '.join(list(function.__code__.co_varnames[:function.__code__.co_argcount][1:]))
-                    description = ' '.join([d.strip() for d in function.__doc__.split('\n')])
-                    self.action_space_prompt = self.action_space_prompt.replace(f"Action Name: {action_name}", "")
-                    self.action_space_prompt = self.action_space_prompt.replace(f"Action Argument: {argument}", "")
-                    self.action_space_prompt = self.action_space_prompt.replace(f"Action Description: {description}", "")
+            action_name = type(action).__name__
+            # Simply remove the action block by its name
+            if action_name in self.action_blocks:
+                del self.action_blocks[action_name]
 
 
 if __name__ == '__main__':
